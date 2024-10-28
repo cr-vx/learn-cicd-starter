79e869bd72c6b7894f5a41a9006f583424834c86
cr-vx <crvx-dev@proton.me>
2024-10-29 02:27:37 +0700

    fix code format

--- internal/auth/auth_test.go
+++ internal/auth/auth_test.go
@@ -1,65 +1,64 @@
 package auth
 
-
 import (
-  "testing"
 	"net/http"
+	"testing"
 )
 
 func TestGetAPIKey(t *testing.T) {
-  tests := []struct {
-    name string
-    input http.Header
-    expectedKey string
-    expectedError string
-  }{
-    {
-      name : "normal header",
-      input : http.Header{
-      "Authorization": []string{"ApiKey 123123123"},
-      },
-      expectedKey: "123123123",
-      expectedError: "",
-    },
+	tests := []struct {
+		name          string
+		input         http.Header
+		expectedKey   string
+		expectedError string
+	}{
+		{
+			name: "normal header",
+			input: http.Header{
+				"Authorization": []string{"ApiKey 123123123"},
+			},
+			expectedKey:   "123123123",
+			expectedError: "",
+		},
 
-    {
-      name: "empty header",
-      input : http.Header{},
-      expectedKey : "",
-      expectedError: "no authorization header included",
-    },
+		{
+			name:          "empty header",
+			input:         http.Header{},
+			expectedKey:   "",
+			expectedError: "no authorization header included",
+		},
 
-    {
-      name: "wrong token",
-      input : http.Header{
-        "Authorization": []string{"Bearer 123123123"},
-      },
-      expectedKey : "",
-      expectedError: "malformed authorization header",
-    },
-    {
-      name: "too short auth header",
-      input : http.Header{
-        "Authorization": []string{"ApiKey"},
-      },
-      expectedKey: "",
-      expectedError: "malformed authorization header",
-    },
-  }
+		{
+			name: "wrong token",
+			input: http.Header{
+				"Authorization": []string{"Bearer 123123123"},
+			},
+			expectedKey:   "",
+			expectedError: "malformed authorization header",
+		},
+		{
+			name: "too short auth header",
+			input: http.Header{
+				"Authorization": []string{"ApiKey"},
+			},
+			expectedKey:   "",
+			expectedError: "malformed authorization header",
+		},
+	}
 
-  for _, tt := range tests {
-    t.Run(tt.name, func(t *testing.T) {
-      got, err := GetAPIKey(tt.input)
-      if tt.expectedError != "" {
-        if err == nil || err.Error() != tt.expectedError {
-          t.Errorf("GetAPIKey() error = %v, expectedError %v", err, tt.expectedError)
-        }
-      } else if err != nil {
-          t.Errorf("GetAPIKey() unexpected error = %v", err)
-        }
-      if got != tt.expectedKey {
-        t.Errorf("GetAPIKey() = %v, want %v", got, tt.expectedKey)
-      }
-    })
-  }
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			got, err := GetAPIKey(tt.input)
+			if tt.expectedError != "" {
+				if err == nil || err.Error() != tt.expectedError {
+					t.Errorf("GetAPIKey() error = %v, expectedError %v", err, tt.expectedError)
+				}
+			} else if err != nil {
+				t.Errorf("GetAPIKey() unexpected error = %v", err)
+			}
+			if got != tt.expectedKey {
+				t.Errorf("GetAPIKey() = %v, want %v", got, tt.expectedKey)
+			}
+		})
+	}
 }
